886ad6d7a2905a25ee475e385e8c8ecd27f3ec79
jiri-hartmann <58955081+jiri-hartmann@users.noreply.github.com>
2020-11-18 16:44:56 +0100

    První funkční prototyp
    Chybí výběr který z počtů osob státu použít.
    Chybí výpočet hustoty zalidnění, ten uváděný v datech vypadá neaktuální.
    Nutná kontrola výsledků na vzorku dat.
    Nutná optimalizace selektu.
    Pro ČR běží 5 sekund bez závěrečného seřazení
    a 30 s pro dotaz (order by cov.date).

---
+++ SQL Project.sql
@@ -0,0 +1,73 @@
+-- pomocné selekty
+select * from covid19_basic_differences ; -- where country = "czechia";
+select * from countries; -- where country = "czech republic";
+select * from covid19_tests ct; -- where iso = "CZE";
+select * from lookup_table where province is null; -- lt where country = "czechia";
+select * from economies where  `year` = 2019; -- e2 where country = "czech republic";
+select * from life_expectancy; -- le where country = "czech republic";
+select * from religions where country = "czech republic" and `year` = 2020;
+select DISTINCT religion from religions order by religion;
+select * from weather;
+select DISTINCT `date` from weather w ;
+
+
+-- zaèátek projektu
+select cov.`date`, case when WEEKDAY(cov.`date`) in (5,6) then 1 else 0 end as is_weekend, 
+    case when month (cov.`date`) in (1,2,3) then 0 
+         when month (cov.`date`) in (4,5,6) then 1
+         when month (cov.`date`) in (7,8,9) then 2
+         else 3 end as season,
+    cov.country, cov.confirmed, tes.tests_performed, loo.population,
+    cou.surface_area, cou.median_age_2018,  
+    round(eco.GDP/1000000) as GDP_mil, eco.gini, eco.mortaliy_under5,
+    round(lif.life_expectancy_difference_2015_1965,2) as "life_expectancy_difference_2015_1965",
+    rel.buddhism, rel.christianity, rel.folk_religions, rel.hinduism, rel.islam, rel.judaism, rel.other_religions, rel.unaffiliated_religions,
+    wea.avg_daily_temp, wea.rainy_hours, wea.max_gust
+from covid19_basic_differences as cov 
+left join 
+    (select country, iso3, population from lookup_table where province is null) as loo
+on cov.country = loo.country
+left join covid19_tests as tes 
+    on cov.`date` = tes.`date` and loo.iso3 = tes.iso
+left join countries as cou
+    on loo.iso3 = cou.iso3
+left join
+    (select country, GDP, gini, mortaliy_under5 from economies where  `year` = 2019) as eco
+    on cou.country = eco.country
+left join
+    (select le2015.country, le2015.life_expectancy-le1965.life_expectancy as "life_expectancy_difference_2015_1965" from 
+    (select country, life_expectancy from life_expectancy where `year` = 2015) as le2015
+    left join
+    (select country, life_expectancy from life_expectancy where `year` = 1965) as le1965
+    on le2015.country = le1965.country) as lif
+on cou.country = lif.country
+left join
+    (select country, 
+        round(sum(if ( religion = "Buddhism", population, Null))/sum(population)*100, 2) as "buddhism", 
+        round(sum(if ( religion = "Christianity", population, Null))/sum(population)*100, 2) as "christianity", 
+        round(sum(if ( religion = "Folk Religions", population, Null))/sum(population)*100, 2) as "folk_religions", 
+        round(sum(if ( religion = "Hinduism", population, Null))/sum(population)*100, 2) as "hinduism", 
+        round(sum(if ( religion = "Islam", population, Null))/sum(population)*100, 2) as "islam", 
+        round(sum(if ( religion = "Judaism", population, Null))/sum(population)*100, 2) as "judaism", 
+        round(sum(if ( religion = "Other Religions", population, Null))/sum(population)*100, 2) as "other_religions", 
+        round(sum(if ( religion = "Unaffiliated Religions", population, Null))/sum(population)*100, 2) as "unaffiliated_religions"
+    from religions where country <> "All Countries" and `year` = 2020 
+    group by country) as rel
+on cou.country = rel.country
+left join
+    (select w.`date`, l.country, w.avg_daily_temp, w.rainy_hours, w.max_gust from
+    (select `date`, city, sum(if(hour in (6, 9, 12, 15), temp, Null)) / 4 as "avg_daily_temp" , sum(if(rain <> 0, 3, 0)) as "rainy_hours", max(gust) as "max_gust"
+    from weather as w
+    where "2020-01-21" < `date` -- 2020-01-22 is first day in incovid19_basic_differences table
+    group by `date`, city ) as w
+   left join 
+    (select capital_city, iso3 from countries) c 
+    on w.city = c.capital_city
+   left join 
+    (select iso3, country from lookup_table lt where province is null) l
+    on c.iso3 = l.iso3 ) as wea
+on cov.`date` = wea.`date` and cov.country = wea.country 
+--  where cov.country = "czechia"            -- "czechia" "United kingdom"
+order by cov.`date`
+  ;
+
